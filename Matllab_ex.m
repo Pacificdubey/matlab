2f307643074111b15f6864e9d91f99cbda6d24cc
Prashant dubey <pacific.prashantdubey@gmail.com>
2022-09-02 08:36:00 +0530

    Add files via upload

---
+++ Matllab_ex.m
@@ -0,0 +1,27 @@
+% function definition
+f = @(x) (x^2)*exp(x)
+
+% interval selection 
+x =linspace(0,5,1000);       % h=1, is selcted as interval between 0 and 5
+
+% loop iteration over interval length to calculate different values 
+for i = 1:length(x)
+     res(i) = f(x(i))       % value stored in res vector for every iteration
+end
+
+%% 
+% Question 2 : find maximum of f(x)
+max_val = max(res);
+disp(max_val)
+
+
+%%
+% Question 3 : find avg of f(x)
+avg_val = mean(res);     % mean function to calculated average
+disp(avg_val)        
+
+%%
+% Question 4: find max of f(x)^2
+max_f_2 = max(res.^2);       % element wise multiplication to calculate f(x)^2
+disp(max_f_2)
+
